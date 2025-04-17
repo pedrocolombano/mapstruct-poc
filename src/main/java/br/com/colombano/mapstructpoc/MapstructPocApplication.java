359064ef6816bfd3cf771b7f401325d9d91f008c
Pedro Colombano <pedromaiacolombano@gmail.com>
2025-04-16 21:48:09 -0300

    feat: create project

---
+++ src/main/java/br/com/colombano/mapstructpoc/MapstructPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.colombano.mapstructpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructPocApplication.class, args);
+    }
+
+}
